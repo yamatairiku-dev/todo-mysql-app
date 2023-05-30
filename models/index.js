9ba8a6966d3d4b0172012c2d213acd08db644411
yamatairiku-dev <yamatairiku.dev@gmail.com>
2023-05-30 19:43:10 +0900

    DB接続改善

--- models/index.js
+++ models/index.js
@@ -8,7 +8,13 @@ const env = process.env.NODE_ENV || 'development'
 const config = require(path.join(__dirname, '/../config/config.json'))[env]
 const db = {}
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config)
+let sequelize
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+  // 例: DB_CONNECTION_URI='mysql://user:passw0rd@mysql-db:3306/todo_app_development'
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config)
+}
 
 fs
   .readdirSync(__dirname)
