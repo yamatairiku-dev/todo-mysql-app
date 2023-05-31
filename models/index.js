f05a5e7c340790676b9abdc68fc987b918464d83
yamatairiku-dev <yamatairiku.dev@gmail.com>
2023-05-31 21:41:41 +0900

    SSL接続試行

--- models/index.js
+++ models/index.js
@@ -7,10 +7,18 @@ const basename = path.basename(__filename) // パスを含まないこのファ�
 const env = process.env.NODE_ENV || 'development'
 const config = require(path.join(__dirname, '/../config/config.json'))[env]
 const db = {}
+const serverCa = [fs.readFileSync('DigiCertGlobalRootCA.crt.pem', 'utf8')]
 
 let sequelize
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialectOptions: {
+      ssl: {
+        ca: serverCa,
+        rejectUnauthorized: true
+      }
+    }
+  })
   // 例: DB_CONNECTION_URI='mysql://user:passw0rd@mysql-db:3306/todo_app_development'
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
